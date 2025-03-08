681569396e5276d6764dfc95e4126132e5706fbe
Alejandro Sanchez <emailforasr@gmail.com>
2025-03-07 17:40:52 -0800

    Vite config updates to proxy to Flask api

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
@@ -12,6 +13,23 @@ export default defineConfig({
     clearMocks: true,
     env: {
       DEBUG_PRINT_LIMIT: '999999',
+    },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000/',
+        changeOrigin: true,
+        rewrite: (path: string) => path,
+        xfwd: true,
+        cookiePathRewrite: '/',
+        cookieDomainRewrite: '',
+      },
     },
   },
 });
