108a00589b714a0607669cb4b35355e96a6e8577
Alejandro Sanchez <emailforasr@gmail.com>
2025-03-07 22:05:44 -0800

    Use tailwind instead of sryled components

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
