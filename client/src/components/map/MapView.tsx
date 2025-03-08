5aa1153b64f31f7c3d1cfcea1720f7970dff149e
Alejandro Sanchez <emailforasr@gmail.com>
2025-03-07 23:02:34 -0800

    Add buffer area circle when clicking the map

--- client/src/components/map/MapView.tsx
+++ client/src/components/map/MapView.tsx
@@ -1,7 +1,8 @@
 import { nanoid } from 'nanoid';
 import React, { useEffect } from 'react';
-import { MapContainer, ScaleControl, TileLayer } from 'react-leaflet';
+import { Circle, MapContainer, ScaleControl, TileLayer } from 'react-leaflet';
 
+import { exists } from '@/utils/utils';
 import { LeafletMouseEvent } from 'leaflet';
 import { FeatureMarker } from './FeatureMarker';
 import { useSearchNearby } from './hooks';
@@ -10,7 +11,7 @@ import { MapEvents } from './MapEvents';
 const victoriaBC = [48.4231, -123.3661];
 
 export const MapView: React.FC<unknown> = () => {
-  const { results, findNearby } = useSearchNearby();
+  const { results, loading, latLng, findNearby } = useSearchNearby();
 
   // get new results whenever the user clicks the map
   const handleMapClick = (event: LeafletMouseEvent) => {
@@ -37,6 +38,13 @@ export const MapView: React.FC<unknown> = () => {
         {results.map((feature) => (
           <FeatureMarker key={nanoid()} feature={feature}></FeatureMarker>
         ))}
+        {loading && exists(latLng) && (
+          <Circle
+            center={latLng}
+            pathOptions={{ fillColor: 'blue' }}
+            radius={500}
+          />
+        )}
       </MapContainer>
     </div>
   );
--- client/src/components/map/hooks.ts
+++ client/src/components/map/hooks.ts
@@ -1,12 +1,18 @@
 import { FeatureCollection } from 'geojson';
+import { LatLngExpression } from 'leaflet';
 import { useCallback, useState } from 'react';
+
 import { PointFeature } from './types';
 
 export const useSearchNearby = () => {
   const [results, setResults] = useState<PointFeature[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [latLng, setLatLng] = useState<LatLngExpression | null>(null);
 
   const findNearby = useCallback(async (lat: number, lon: number) => {
     try {
+      setLoading(true);
+      setLatLng({ lat: lat, lng: lon });
       const url = new URL('/api/places', document.baseURI);
       url.searchParams.set('lat', lat.toString());
       url.searchParams.set('lon', lon.toString());
@@ -14,11 +20,14 @@ export const useSearchNearby = () => {
       const apiResponse = await fetch(url.toString());
       const data: FeatureCollection = await apiResponse.json();
       setResults(data.features as PointFeature[]);
+      setLoading(false);
     } catch (error) {
       setResults([]);
+      setLoading(false);
+      setLatLng(null);
       console.error(error);
     }
   }, []);
 
-  return { results, findNearby };
+  return { results, loading, latLng, findNearby };
 };
